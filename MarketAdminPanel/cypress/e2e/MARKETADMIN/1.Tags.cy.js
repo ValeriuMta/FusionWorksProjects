53708ec1b3b2c74c7258a737add4ac9dca5439d4
ValeriuMta <vmereuta@fusionworks.md>
2023-12-05 20:20:50 +0200

    Last updates

--- MarketAdminPanel/cypress/e2e/MARKETADMIN/1.Tags.cy.js
+++ MarketAdminPanel/cypress/e2e/MARKETADMIN/1.Tags.cy.js
@@ -1,44 +1,44 @@
 ///<reference types="cypress" />
-// Tag Creation on admin panel 
+// Tag Creation on admin panel
 
 function makeid(length) {
-    let result = '';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const charactersLength = characters.length;
-    let counter = 0;
-    while (counter < length) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-      counter += 1;
-    }
-    return result;
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
 }
 
+it("Tag Creation", function () {
+  cy.visit("https://market-admin.stg.dely.io/");
 
-it ('Tag Creation', function() {
+  //login
+  cy.get("#mui-6").type("superadmin@ifood.md");
+  cy.get("#mui-7").type("12345678");
+  cy.get(".MuiButton-root").click();
 
-cy.visit('https://market-admin.stg.dely.io/')
+  //TAGS creation
+  cy.contains("div.MuiButtonBase-root", "CMS")
+    .next()
+    .find("a.MuiMenuItem-root")
+    .contains("Tags")
+    .click();
 
-//login
-cy.get('#mui-6').type('superadmin@ifood.md')
-cy.get('#mui-7').type('12345678')
-cy.get('.MuiButton-root').click()
+  //click on create
+  cy.get("a.MuiButton-root").click();
 
+  const name = makeid(5);
+  cy.get("#name").type(name);
 
-//TAGS creation
-cy.contains('div.MuiButtonBase-root', "CMS").next().find("a.MuiMenuItem-root").contains("Tags").click()
+  for (let i = 0; i < 2; i++) {
+    cy.get(`#translations\\[label_${i}\\]`).type(name);
+  }
 
-//click on create
-cy.get('a.MuiButton-root').click()
-
-
-const name = makeid(5)
-cy.get('#name').type(name)
-
-for ( let i = 0; i < 2; i++ ){
-cy.get(`#translations\\[label_${i}\\]`).type(name)}
-
-cy.get('#slug').type(name)
-cy.get('button[aria-label="Save"]').click()
-
-})
-
+  cy.get("#slug").type(name);
+  cy.get('button[aria-label="Save"]').click();
+});
