82bbe0ea09f73ab984709483d264191759e316b1
ValeriuMta <vmereuta@fusionworks.md>
2024-01-18 22:00:30 +0200

    Last changes Create requests

--- MSD/cypress/e2e/MSD/CreateRequest.cy.js
+++ MSD/cypress/e2e/MSD/CreateRequest.cy.js
@@ -1,35 +1,46 @@
+import { IgnoreUncaughtException } from "../../../Support-errors/uncaught-exception.cy.js"
 ///<reference types="cypress" />
 
-import { IgnoreUncaughtException } from "../../../Support-errors/uncaught-exception.cy.js"
-
-//random cifre
-const numbr= Math.floor(Math.random() * 900) + 100;
-//random litere
-const txtr = Math.random().toString(36).substring(2, 7);
-
-it('Create reques', function() {
+function makeName(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+  }
+  
+  
+  it ('Suppliers Creation', function() {
     IgnoreUncaughtException()
-    cy.visit('https://admin.msd.dely.io/login')
+      cy.visit('https://admin.msd.dely.io/login')
+      
+      //login
+      cy.get('#mui-1').type("msdmanager@dely.io")
+      cy.get('#mui-2').type("12345678")
+      cy.get('button.MuiButton-root').click()
 
-    //login
-    cy.get('#mui-1').type('msdmanager@dely.io')
-    cy.get('#mui-2').type('12345678')
+      //choose organization
+      cy.get('button.MuiButtonBase-root').contains("Test MD").click()
 
-    cy.get('.MuiButton-root').click()
+      //click on create request
+      cy.get('a.MuiMenuItem-root').contains("Create Request").click()
 
-    //choose organisation 
-    cy.get('body > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1)').click()
+      //click on create
+      cy.get('a.MuiButton-root').contains("Create").click()
+
+      //fill all the inputs
+      const name1 = makeName(5)
+      cy.get('#orderId').type(name1)
+      cy.get('#totalSum').type("211")
+      cy.get('#customer\\.\\firstName').type("TEsting")
+      cy.get('#customer.lastName').type("Mta")
+      cy.get('#customer.phoneNumber').type(+37390909090)
+      cy.get('div.MuiFilledInput-root').find('button.MuiButtonBase-root').click()
+
+
     
-
-    //create-request
-    cy.get('[href="/delivery/request-delivery"]').click()
-
-    //+create
-    cy.get("a[aria-label='Create']").click()
-
-
-    //orderid
-    cy.get('#orderId').type(numbr)
-    
-    
-})
+    })
