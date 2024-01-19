5001e895e15e35d76a3a408b9f765d1abb39a2ac
ValeriuMta <vmereuta@fusionworks.md>
2024-01-19 18:56:33 +0200

    CreateRequest last updates

--- MSD/cypress/e2e/MSD/CreateRequest.cy.js
+++ MSD/cypress/e2e/MSD/CreateRequest.cy.js
@@ -35,11 +35,42 @@ function makeName(length) {
       //fill all the inputs
       const name1 = makeName(5)
       cy.get('#orderId').type(name1)
+      cy.get('#date').type("2024-02-28")
       cy.get('#totalSum').type("211")
       cy.get('#customer\\.firstName').type("TEsting")
       cy.get('#customer\\.lastName').type("Mta")
       cy.get('#customer\\.phoneNumber').type(+37390909090)
       cy.get('div.MuiFilledInput-root').find('button.MuiButtonBase-root').click()
+      cy.get('#restaurantId-option-0').click()
+      cy.get('#deliveryAddress\\.street').type("3")
+      cy.get('#deliveryAddress\\.streetNumber').type("22")
+      cy.get('#deliveryAddress\\.locality').type(name1)
+      cy.get('#deliveryAddress\\.area').type(name1)
+      cy.get('#deliveryAddress\\.country').type(name1)
+      cy.get('#deliveryAddress\\.buildingNumber').type(name1)
+      cy.get('#deliveryAddress\\.intercom').type(name1)
+      cy.get('#deliveryAddress\\.entrance').type(name1)
+      cy.get('#deliveryAddress\\.floor').type(name1)
+      cy.get('#deliveryAddress\\.apartment').type(name1)
+      cy.get('#deliveryAddress\\.extraInfo').type(name1)
+      cy.get('#deliveryAddress\\.formattedValue').type(name1)
+      cy.get('#deliveryAddress\\.geo\\.lat').type("100")
+      cy.get('#deliveryAddress\\.geo\\.lng').type("200")
+      cy.get('#paymentMethod').click()
+      cy.contains('li.MuiMenuItem-root',"CREDIT CARD").click()
+      cy.get('#paymentStatus').click()
+      cy.get('ul.MuiList-root').contains('li.MuiMenuItem-root',"SUCCESS").click()
+      cy.get('button.MuiButton-root').contains("Add").click()
+      cy.get('#products\\.0\\.id').type(name1)
+      cy.get('#products\\.0\\.name').type(name1)
+      cy.get('#products\\.0\\.totalPrice').type("100")
+      cy.get('#products\\.0\\.basePrice').type("50")
+      cy.get('#products\\.0\\.quantity').type("5")
+      cy.get('button.MuiButton-root').contains("Save").click()
+
+
+
+
 
 
     
