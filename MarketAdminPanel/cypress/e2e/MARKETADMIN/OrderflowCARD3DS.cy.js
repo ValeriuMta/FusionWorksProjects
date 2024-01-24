aa9c66bd99048d2240c8bb80fe07c2d4c6d04789
ValeriuMta <vmereuta@fusionworks.md>
2024-01-24 20:12:42 +0200

    las changes card payment 3DS

--- MarketAdminPanel/cypress/e2e/MARKETADMIN/OrderflowCARD3DS.cy.js
+++ MarketAdminPanel/cypress/e2e/MARKETADMIN/OrderflowCARD3DS.cy.js
@@ -43,12 +43,18 @@ cy.get('.ng-untouched').find('div.payment-items').eq(1).click()
 
 //check box
 cy.get('div.terms-container').find('input[type = "checkbox"]').check({ force: true })
+
+
 cy.get('div.margin-horizontal').find('button[type = "undefined"]').click()
 cy.wait(1000)
 cy.get('div.margin-horizontal').find('button[type = "undefined"]').click()
 cy.wait(1500)
-// cy.origin('https://stripe.com/sources/test_source_3ds?livemode=false&type=three_d_secure&pass_through=&return_url=https%3A%2F%2Fhooks.stripe.com%2Fthree_d_secure%2Fredirect_complete%2Facct_1NfgN3In9mRh1xUY%2Ftdsrc_1Oc3xRIn9mRh1xUY8fRnyLYu&source_slug=CBsaFwoVYWNjdF8xTmZnTjNJbjltUmgxeFVZKNvVw60GMgbIaaQV2pc6LdhvQ9N3ea0KxHBC_oqJyNeuMvkLpa3X4Fy2wK2IAB4O19rgjNURoUbkAuWaBg&amount=10600&currency=dkk&usage=single_use')
-cy.get('div.common-ButtonGroup').contains('#test-source-authorize-3ds',"Complete authentication").click()
+  
+cy.origin("https://stripe.com/sources/test_source_3ds?livemode=false&type=three_d_secure&pass_through=&return_url=https%3A%2F%2Fhooks.stripe.com%2Fthree_d_secure%2Fredirect_complete%2Facct_1NfgN3In9mRh1xUY%2Ftdsrc_1Oc8ccIn9mRh1xUYkFcIhvCf&source_slug=CBsaFwoVYWNjdF8xTmZnTjNJbjltUmgxeFVZKObqxK0GMgb2I3hNA0g6LdgJ-DzcF7bQpL3_g_9CfqrAvTK42JLD0Wb0evpzcnJyBsDKHUQ1tPBYdtg_9A&amount=10600&currency=dkk&usage=single_use",() =>{
+  cy.get('#test-source-authorize-3ds').contains("Complete authentication").click()
+  
+})  
+
 cy.wait(1500)
 cy.get('app-button.col').find('button[type = "undefined"]').click()
 
